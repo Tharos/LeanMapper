fe6534e4036eafab9830dc111ff5fc196369b989
vojtech_kohout <v.kohout@gmail.com>
2013-05-30 16:30:11 +0200

    Added license informations

--- LeanMapper/Entity.php
+++ LeanMapper/Entity.php
@@ -1,9 +1,12 @@
 <?php
 
 /**
- * This file is part of the Lean Mapper library
+ * This file is part of the Lean Mapper library (http://www.leanmapper.com)
  *
  * Copyright (c) 2013 Vojtěch Kohout (aka Tharos)
+ *
+ * For the full copyright and license information, please view the file
+ * license-mit.txt that was distributed with this source code.
  */
 
 namespace LeanMapper;
