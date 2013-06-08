5e33ea99f7fb3ff381db43be2b58d7dbd0d6081c
vojtech_kohout <v.kohout@gmail.com>
2013-06-08 17:26:21 +0200

    Added Nette Tester tests infrastructure

---
+++ tests/bootstrap.php
@@ -0,0 +1,32 @@
+<?php
+
+if (@!include __DIR__ . '/../vendor/autoload.php') {
+	echo 'Install Nette Tester using `composer update --dev`';
+	exit(1);
+}
+
+require_once __DIR__ . '/lib.inc.php';
+
+// configure environment
+Tester\Helpers::setup();
+class_alias('Tester\Assert', 'Assert');
+date_default_timezone_set('Europe/Prague');
+
+$_SERVER = array_intersect_key($_SERVER, array_flip(array('PHP_SELF', 'SCRIPT_NAME', 'SERVER_ADDR', 'SERVER_SOFTWARE', 'HTTP_HOST', 'DOCUMENT_ROOT', 'OS', 'argc', 'argv')));
+$_SERVER['REQUEST_TIME'] = 1234567890;
+$_ENV = $_GET = $_POST = array();
+
+if (extension_loaded('xdebug')) {
+	xdebug_disable();
+	Tester\CodeCoverage\Collector::start(__DIR__ . '/coverage.dat');
+}
+
+if (!copy(__DIR__ . '/db/library-ref.sq3', __DIR__ . '/db/library.sq3')) {
+	echo 'Failed to copy SQLite database';
+	exit(1);
+}
+
+$connection = new DibiConnection(array(
+	'driver' => 'sqlite3',
+	'database' => __DIR__ . '/db/library.sq3',
+));
